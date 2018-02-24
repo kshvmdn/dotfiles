74c737b4c2716f31883d912890ae5ebedfca1be0
Kashav Madan <kshvmdn@gmail.com>
2018-02-23 21:29:49 -0500

    Clean-up vim config., use vim-plug instead of pathogen

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
-mkdir -p "$DIR"/.vim/{autoload,bundle,tmp/{backup,swap,undo}} &&
-  curl -LSso "$DIR"/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-ln -sf "$DIR"/.vim "$HOME"/.vim
-ln -sf "$DIR"/.vimrc "$HOME"/.vimrc
+
+mkdir -p "$HOME"/.vim/{autoload,tmp/{backup,swap,undo}}
+
+curl -fLo "$HOME"/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -sf "$DIR"/vimrc "$HOME"/.vim/vimrc
+vim +PlugInstall +qall
